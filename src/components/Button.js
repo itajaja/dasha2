dd6fd8e2d7a636db9d40c5f45579f11af0e0cfb4
Giacomo Tagliabue <giacomo.tag@gmail.com>
2022-01-05 11:25:58 -0500

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+export default function Button(props) {
+  return (
+    <button {...props} className="d-button" />
+  );
+}
