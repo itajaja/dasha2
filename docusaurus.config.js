7ba4654286d91a8922e445153d5a667299dc1b68
Giacomo Tagliabue <giacomo.tag@gmail.com>
2022-01-05 11:44:52 -0500

    configure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Dasha Filippova',
+  tagline: '',
+
+  organizationName: "itajaja",
+  projectName: "dasha2",
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+  url: 'http://darjafilippova.com',
   baseUrl: '/',
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   presets: [
     [
